9eccf8d3f6b0af2667433e577c819378865cf3f2
Hal Vong <halvong@yahoo.com>
2020-05-24 11:50:08 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+Spring & Spring Boot, Udemy Video
+
+May 24, 2020, Sun
+
+Adding Bootstrap and custom css file
+
+#url
+https://www.udemy.com/course/spring-and-spring-boot-learn-by-bulding-projects/learn/lecture/15459666#overview
---
+++ src/main/java/com/vojislavk/cmsshoppingcart/Controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.vojislavk.cmsshoppingcart.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/aa")//look in WebConfig
+    public String home() {
+        return "home";
+    }
+
+}
